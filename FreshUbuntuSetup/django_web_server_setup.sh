1695a3993446da1c2818ebed8a009c9e2b99e5fd
Your Name <you@example.com>
2017-12-01 10:34:37 +0800

    add

--- FreshUbuntuSetup/django_web_server_setup.sh
+++ FreshUbuntuSetup/django_web_server_setup.sh
@@ -3,46 +3,24 @@
 
 # install pip3
 sudo apt update
-sudo apt install python3-pip
-
-# change pip source to tsinghua mirror
-pip_config_file=${HOME}"/.pip/pip.cnf"
-pip_dir=${HOME}"/.pip"
-echo ${pip_config_file}
-
-if [ -f ${pip_config_file} ]
-then
-	echo "pip.cnf aleady exist."
-	:> ${pip_config_file}
-	echo "[global]" >> ${pip_config_file}
-	echo "index-url = https://pypi.tuna.tsinghua.edu.cn/simple" >> ${pip_config_file}
-	echo "[install]" >> ${pip_config_file}
-	echo "trusted-host=mirrors.aliyun.com" >> ${pip_config_file}
-else
-	mkdir ${pip_dir}
-	touch ${pip_config_file}
-	echo "[global]" >> ${pip_config_file}
-	echo "index-url = https://pypi.tuna.tsinghua.edu.cn/simple" >> ${pip_config_file}
-	echo "[install]" >> ${pip_config_file}
-	echo "trusted-host=mirrors.aliyun.com" >> ${pip_config_file}
-fi
-echo "pip conf file established success!"
+sudo apt install -i python3-pip
 
 # install oh-my-zsh
-sudo apt install curl
-sudo apt install wget
-sudo apt install zsh
+sudo apt -y install curl
+sudo apt -y install wget
+sudo apt -y install zsh
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
 sudo chsh -s /bin/zsh
 
 # install nginx and supervisor and postgresql
-sudo apt install nginx
-sudo apt install supervisor
-sudo apt install postgresql
-sudo apt install libpq-dev
+sudo apt -y install nginx
+sudo apt -y install supervisor
+sudo apt -y install postgresql
+sudo apt -y install libpq-dev
 
 # fix tencent-cloud issue
-# export LC_ALL=C
+echo export LC_ALL=C >> ~/.bashrc
+source ~/.bashrc
 
 # install python3 packages
 sudo pip3 install django
