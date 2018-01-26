d8a6beeab9a8f5f2fb16d9775d0ea4c7a36c21e5
Your Name <you@example.com>
2018-01-26 19:23:06 +0800

    add

--- UbuntuInstaller/install_go1.9.sh
+++ UbuntuInstaller/install_go1.9.sh
@@ -7,6 +7,7 @@ sudo apt-get install -y golang-go
 
 go get gitlab.com/jinfagang/colorgo
 git clone https://github.com/golang/net.git $HOME/go/src/golang.org/x/net
+git clone https://github.com/golang/sys.git $HOME/go/src/golang.rog/x/sys
 
 GOVERSION=`go version`
 echo ${GOVERSION}' fucking install success!'
