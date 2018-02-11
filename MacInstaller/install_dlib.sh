e9e91dd514a2c21eaa046b7cac1ecdd3c7462362
jinfagang <nicholasjela@gmail.com>
2018-02-11 12:50:07 +0800

    add

---
+++ MacInstaller/install_dlib.sh
@@ -0,0 +1,10 @@
+brew cask install xquartz
+brew install gtk+3 boost
+brew install boost-python --with-python3 --without-python
+
+
+brew link python3
+
+brew install dlib
+sudo pip3 install dlib
+echo "Now dlib should be installed successfully!"
