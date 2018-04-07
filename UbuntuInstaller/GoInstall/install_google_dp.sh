835dd330cc5dd5ab6e110c6a942f7887751ec920
Your Name <you@example.com>
2018-04-07 13:23:05 +0800

    add text

--- UbuntuInstaller/GoInstall/install_google_dp.sh
+++ UbuntuInstaller/GoInstall/install_google_dp.sh
@@ -4,3 +4,4 @@ git clone https://github.com/golang/sys.git $HOME/go/src/golang.org/x/sys
 git clone https://github.com/golang/crypto.git $HOME/go/src/golang.org/x/crypto
 git clone https://github.com/golang/tools.git $HOME/go/src/golang.org/x/tools
 git clone https://github.com/grpc/grpc-go.git $HOME/go/src/google.golang.org/grpc
+git clone https://github.com/golang/tools.git $HOME/go/src/golang.org/x/text
