f7c5ac109eb373652b90b4e01d7d2ac3bacb663e
Your Name <you@example.com>
2018-09-13 08:50:40 +0800

    add

--- init_ubuntu.sh
+++ init_ubuntu.sh
@@ -31,6 +31,13 @@ sudo apt install -y curl
 sudo apt install -y axel
 sudo apt install -y wget
 
+echo '============= installing sublime =============================='
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get update
+sudo apt-get install -y sublime-text
+
+
 echo "=========installing shadowsocks-qt5-Break Wall!=========="
 sudo add-apt-repository -y  ppa:hzwhuang/ss-qt5
 sudo apt-get update
