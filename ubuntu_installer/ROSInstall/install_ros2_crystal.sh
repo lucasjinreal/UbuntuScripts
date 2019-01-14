4ef774f44e0626b2fa5ca162770a81e2b1753a59
Your Name <you@example.com>
2019-01-14 10:43:01 +0800

    add ros2 install scripts

---
+++ ubuntu_installer/ROSInstall/install_ros2_crystal.sh
@@ -0,0 +1,11 @@
+sudo apt update && sudo apt install curl gnupg2 lsb-release
+curl http://repo.ros2.org/repos.key | sudo apt-key add -
+
+sudo sh -c 'echo "deb [arch=amd64,arm64] http://packages.ros.org/ros2/ubuntu `lsb_release -cs` main" > /etc/apt/sources.list.d/ros2-latest.list'
+
+sudo apt update
+sudo apt install ros-crystal-desktop
+sudo apt install ros-crystal-ros-base
+sudo apt install python3-argcomplete
+
+echo 'Congratulations! you are installed ros2 crystal!'
