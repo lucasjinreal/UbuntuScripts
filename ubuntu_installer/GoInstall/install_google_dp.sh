fea1308e1fe3a9ea6af562b5fb94c4327fa639b2
xxxxxx <xxxx@.com>
2019-04-19 23:09:48 +0800

    add

--- ubuntu_installer/GoInstall/install_google_dp.sh
+++ ubuntu_installer/GoInstall/install_google_dp.sh
@@ -1,4 +1,11 @@
 go get gitlab.com/jinfagang/colorgo
+
+go get github.com/gorilla/schema
+go get github.com/gomodule/redigo/redis
+go get github.com/go-sql-driver/mysql
+go get github.com/lib/pq
+
+
 git clone https://github.com/golang/net.git $HOME/go/src/golang.org/x/net
 git clone https://github.com/golang/sys.git $HOME/go/src/golang.org/x/sys
 git clone https://github.com/golang/crypto.git $HOME/go/src/golang.org/x/crypto
