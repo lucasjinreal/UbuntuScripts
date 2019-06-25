85841c4967617b3b26374b720fd32a07c8a007cd
Your Name <you@example.com>
2019-06-25 17:30:42 +0800

    add install ros

---
+++ favor_scripts/install_ros_pkgs.sh
@@ -0,0 +1,19 @@
+ROS_DIST='melodic'
+
+
+sudo apt install -y ros-${ROS_DIST}-jsk-recognition
+sudo apt install -y ros-${ROS_DIST}-roscpp
+sudo apt install -y ros-${ROS_DIST}-pcl-conversions
+sudo apt install -y ros-${ROS_DIST}-pcl-ros
+sudo apt install -y ros-${ROS_DIST}-tf-conversions
+sudo apt install -y ros-${ROS_DIST}-laser-geometry
+sudo apt install -y ros-${ROS_DIST}-cv-bridge
+sudo apt install -y ros-${ROS_DIST}-nav-msgs
+sudo apt install -y ros-${ROS_DIST}-visualization-msgs
+sudo apt install -y ros-${ROS_DIST}-image-transport
+sudo apt install -y ros-${ROS_DIST}-geodesy
+#sudo apt install -y ros-${ROS_DIST}-cv-bridge
+
+sudo apt install -y ros-${ROS_DIST}-image-transport
+sudo apt install -y ros-${ROS_DIST}-geodesy
+sudo apt install -y ros-${ROS_DIST}-roscpp
