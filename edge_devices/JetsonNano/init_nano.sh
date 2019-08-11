9eb9610ff1b2892c58ff8979468425c80a412a7b
Your Name <you@example.com>
2019-08-11 19:42:14 +0800

    add

---
+++ edge_devices/JetsonNano/init_nano.sh
@@ -0,0 +1,10 @@
+#
+#
+# this script will install dependencies needed by nano
+# for deep learning purpose
+#
+
+
+sudo apt install -y libboost-all-dev
+sudo apt install -y libcurl4-openssl-dev
+sudo apt install -y libopencv-dev
