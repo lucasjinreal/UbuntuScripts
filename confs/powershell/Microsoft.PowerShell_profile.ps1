58de9b1a57a8861db839139b45a56e17ad1846cc
KK <1@@q.com>
2020-10-04 09:10:20 +0800

    add powershell config file

---
+++ confs/powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,14 @@
+Import-Module posh-git
+Import-Module oh-my-posh
+Import-Module PSReadLine
+
+# Set-Theme Honukai
+# Set-Theme Fish
+# Set-Theme Paradox
+Set-Theme Zash
+
+Set-PSReadlineKeyHandler -Key Tab -Function Complete # 设置 Tab 键补全
+Set-PSReadLineKeyHandler -Key "Ctrl+d" -Function MenuComplete # 设置 Ctrl+d 为菜单补全和 Intellisense
+Set-PSReadLineKeyHandler -Key "Ctrl+z" -Function Undo # 设置 Ctrl+z 为撤销
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward # 设置向上键为后向搜索历史记录
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward # 设置向下键为前向搜索历史纪录
