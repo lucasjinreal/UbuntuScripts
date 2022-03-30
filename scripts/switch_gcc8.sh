5b4b762fbfc7cb6e3527cc522e892de18d4ef670
Your Name <you@example.com>
2022-03-30 19:19:22 +0800

    add

--- scripts/switch_gcc8.sh
+++ scripts/switch_gcc8.sh
@@ -1,8 +1,8 @@
  sudo rm /usr/bin/gcc
- sudo ln -s /usr/bin/gcc-7 /usr/bin/gcc
+ sudo ln -s /usr/bin/gcc-8 /usr/bin/gcc
 
  sudo rm /usr/bin/g++
- sudo ln -s /usr/bin/g++-7 /usr/bin/g++
+ sudo ln -s /usr/bin/g++-8 /usr/bin/g++
 
  sudo ldconfig
 
